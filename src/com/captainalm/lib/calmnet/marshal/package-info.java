d4726c553c1b3737823fafe5fbf19157543dfa7a
Captain ALM <captainalm@captainalm.com>
2022-06-21 22:18:14 +0100

    Add NetMarshal TODO list.

--- src/com/captainalm/lib/calmnet/marshal/package-info.java
+++ src/com/captainalm/lib/calmnet/marshal/package-info.java
@@ -4,3 +4,8 @@
  * @author Captain ALM
  */
 package com.captainalm.lib.calmnet.marshal;
+/*TODO:
+NetMarshalClientWrapped - Stream wrapping support
+NetMarshalServer - Has a thread for UDP receiving and has a dictionary of input streams (final, created in constructor)
+NetMarshalServerWrapped - Constructs NetMarshalClientWrapped instead of NetMarshalClient, stream wrapping support
+ */
